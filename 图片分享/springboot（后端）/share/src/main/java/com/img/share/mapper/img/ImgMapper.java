b631473105774f16044a2ae8f9f8c5ed71a10fcb
arknights <freeman.lnu@outlook.com>
2024-07-09 22:03:43 +0800

    分页查询修改

--- 图片分享/springboot（后端）/share/src/main/java/com/img/share/mapper/img/ImgMapper.java
+++ 图片分享/springboot（后端）/share/src/main/java/com/img/share/mapper/img/ImgMapper.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 
 import com.img.share.pojo.Img;
@@ -22,6 +24,10 @@ public interface ImgMapper {
     @Select("select count(*) from img")
     public Integer getCount();
     /* 分页查 */
-    @Select("select * from img order by #{order} limit #{start},#{count}")
+    @Select("select i.iid,i.iname,i.isrc,i.uploaddate,i.pageview,u.uid,u.uname from img i left join `user` u on i.uid = u.uid order by #{order} limit #{start},#{count}")
+    @Results({
+        @Result(property = "user.uid",column = "uid"),
+        @Result(property = "user.uname",column = "uname")
+    })
     public List<Img> search(String order,Integer start,Integer count); 
 }
