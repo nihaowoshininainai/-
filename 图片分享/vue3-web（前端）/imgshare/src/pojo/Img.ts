0942a1c8db195feb0657469a7338a1b81503ef84
arknights <freeman.lnu@outlook.com>
2024-07-16 20:05:43 +0800

    修改Img类

--- 图片分享/vue3-web（前端）/imgshare/src/pojo/Img.ts
+++ 图片分享/vue3-web（前端）/imgshare/src/pojo/Img.ts
@@ -1,12 +1,12 @@
 import type { Commentt } from "./Commentt"
-import type { User } from "./User"
+import { User } from "./User"
 
 export class Img {
     iid: number = -1
     iname: string = ''
     isrc: string = ''
     uploaddate: string = ''
-    user: User | null = null
+    user: User = new User()
     pageview: number = 0
     comment: Commentt[] = []
     likeUsers: User[] = []
