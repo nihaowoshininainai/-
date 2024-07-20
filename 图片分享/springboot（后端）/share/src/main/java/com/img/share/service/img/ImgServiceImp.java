8c4cce3703a4772553281c3d0dd6743541327c9d
arknights <freeman.lnu@outlook.com>
2024-07-20 18:53:29 +0800

    添加图片功能设置为多线程，IO操作与写入数据库操作互不影响

--- 图片分享/springboot（后端）/share/src/main/java/com/img/share/service/img/ImgServiceImp.java
+++ 图片分享/springboot（后端）/share/src/main/java/com/img/share/service/img/ImgServiceImp.java
@@ -13,59 +13,86 @@ import org.springframework.web.multipart.MultipartFile;
 import com.img.share.mapper.img.ImgMapper;
 import com.img.share.pojo.Img;
 import com.img.share.pojo.Statues;
+
 @Service
-public class ImgServiceImp implements ImgService{
-    public static final String FILEDIR = "/mnt/img/";
-    /* public static final String FILEDIR = "D:/c/img"; */
+public class ImgServiceImp implements ImgService {
+    /* public static final String FILEDIR = "/mnt/img/"; */
+    public static final String FILEDIR = "D:/c/img";
     @Autowired
     private ImgMapper imgMapper;
 
-    @Override
-    public Statues<Integer> add(MultipartFile file,String iname,Integer uid) throws IllegalStateException, IOException {
+    class AddImg extends Thread {
+        MultipartFile file;
+        String iname;
+        Integer uid;
         long time = System.currentTimeMillis();
+
+        public AddImg(MultipartFile file, String iname, Integer uid) {
+            this.file = file;
+            this.iname = iname;
+            this.uid = uid;
+        }
+
+        @Override
+        public void run() {
+            @SuppressWarnings("null")
+            String filepath = FILEDIR + this.uid + "/" + this.iname + this.time
+                    + this.file.getOriginalFilename().substring(this.file.getOriginalFilename().lastIndexOf("."));
+            File newFile = new File(filepath);
+            File dir = newFile.getParentFile();
+            if (!dir.exists()) {
+                // 创建文件夹
+                dir.mkdirs();
+            }
+            // 创建文件
+            try {
+                file.transferTo(newFile);
+            } catch (IllegalStateException | IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    @Override
+    public Statues<Integer> add(MultipartFile file, String iname, Integer uid)
+            throws IllegalStateException, IOException {
+        AddImg addImg = new AddImg(file, iname, uid);
+        addImg.start();
         @SuppressWarnings("null")
-        String filepath = FILEDIR+uid+"/"+iname+time+file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
-        File newFile = new File(filepath);
-        File dir = newFile.getParentFile();
-        if (!dir.exists()) {
-            // 创建文件夹
-            dir.mkdirs();
-        }
-        // 创建文件
-        file.transferTo(newFile);
-        System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(time)));
-        Integer a = imgMapper.add(iname,filepath,new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(time)),uid);
-        if(a!=1){
-            return new Statues<Integer>(0,"添加图片失败",null);
-        }
-        else{
-            return new Statues<Integer>(1,"添加图片成功",null);
+        String filepath = FILEDIR + addImg.uid + "/" + addImg.iname + addImg.time
+                + addImg.file.getOriginalFilename().substring(addImg.file.getOriginalFilename().lastIndexOf("."));
+        System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(addImg.time)));
+        Integer a = imgMapper.add(addImg.iname, filepath,
+                new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(addImg.time)), addImg.uid);
+        if (a != 1) {
+            return new Statues<Integer>(0, "添加图片失败", null);
+        } else {
+            return new Statues<Integer>(1, "添加图片成功", null);
         }
     }
 
     @Override
     public Statues<Integer> delete(Integer iid, Integer uid) {
         Integer a = imgMapper.delete(iid, uid);
-        if(a==0){
-            return new Statues<>(0,"删除失败",null);
-        }
-        else{
-            return new Statues<>(1,"删除成功",null);
+        if (a == 0) {
+            return new Statues<>(0, "删除失败", null);
+        } else {
+            return new Statues<>(1, "删除成功", null);
         }
 
     }
 
     @Override
     public Statues<List<Img>> search(String order, Integer page, Integer count) {
-        String message = "这是第"+page+"页";
-        System.out.println(imgMapper.search(order, (page-1)*count, count));
-        return new Statues<>(1,message,imgMapper.search(order, (page-1)*count, count));
+        String message = "这是第" + page + "页";
+        System.out.println(imgMapper.search(order, (page - 1) * count, count));
+        return new Statues<>(1, message, imgMapper.search(order, (page - 1) * count, count));
     }
 
     @Override
-    public Statues<Integer> getCount(){
-        return new Statues<>(1,"获取成功",imgMapper.getCount());
+    public Statues<Integer> getCount() {
+        return new Statues<>(1, "获取成功", imgMapper.getCount());
     }
-    
 
 }
