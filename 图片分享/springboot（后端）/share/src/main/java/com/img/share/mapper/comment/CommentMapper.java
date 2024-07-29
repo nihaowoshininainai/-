09ae17eaffa71ac89924464af3812b5f7aa8742e
arknights <freelnu@163.com>
2024-07-29 15:10:31 +0800

    修改查询评论接口，增加获取评论用户的相关信息

--- 图片分享/springboot（后端）/share/src/main/java/com/img/share/mapper/comment/CommentMapper.java
+++ 图片分享/springboot（后端）/share/src/main/java/com/img/share/mapper/comment/CommentMapper.java
@@ -5,18 +5,27 @@ import java.util.List;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 
 import com.img.share.pojo.Comment;
+
 @Mapper
 public interface CommentMapper {
-    /*查 */
-    @Select("select * from comment c where iid = #{iid}")
+    /* 查 */
+    @Select("select c.cid,c.iid,c.commdate,u.uid,u.uname,c.clicklike,c.content from comment c left join `user` u on c.uid = u.uid where iid = #{iid}")
+    @Results({
+            @Result(property = "user.uid", column = "uid"),
+            @Result(property = "user.uname", column = "uname")
+    })
     public List<Comment> getComments(Integer iid);
-    /*增 */
+
+    /* 增 */
     @Insert("insert into comment(iid,commdate,uid,content) values(#{iid},#{commdate},#{uid},#{content})")
-    public Integer addComment(Integer iid,String commdate,Integer uid,String content);
-    /*删 */
+    public Integer addComment(Integer iid, String commdate, Integer uid, String content);
+
+    /* 删 */
     @Delete("delete from comment where cid=#{cid}")
     public Integer delComment(Integer cid);
 }
